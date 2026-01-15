f58c967c59c3ecbb53661552f35a3ca6a16f79b2
Tran Hung Thinh <124481874+little-fox28@users.noreply.github.com>
2026-01-16 06:00:59 +0700

    hot-fix: change products page to careers page

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import NewsPage from './pages/NewsPage';
 import CareersPage from './pages/CareersPage';
-import ProductsPage from './pages/ProductsPage';
 import FloatingContact from './components/common/FloatingContact';
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
     <Router basename="/wataco/">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/careers" element={<CareersPage />} />
         <Route path="/news" element={<NewsPage />} />
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const { t, lang, setLang, icons } = useTranslation();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
-  const navLinks = ["/", "/projects", "/products", "/news"];
+  const navLinks = ["/", "/projects", "/careers", "/news"];
 
   return (
     <div className="bg-[#F4F7F6] text-[#1A2B3C] selection:bg-[#228B22] selection:text-white">
