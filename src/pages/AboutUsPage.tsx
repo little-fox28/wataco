71d22f5cb6e23097239f5fcf24b0ef2a6da7f83e
Tran Hung Thinh <124481874+little-fox28@users.noreply.github.com>
2026-04-23 19:05:45 +0700

    update

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -54,9 +54,9 @@ const StaggerItem = forwardRef<HTMLDivElement, { children: ReactNode; className?
 ));
 
 const dailyActivitiesMeta = [
-    { id: 1, img: "/team_building.jpg", span: "md:col-span-2 md:row-span-2" },
-    { id: 2, img: "https://images.unsplash.com/photo-1759922378222-47ad736a174d?auto=format&fit=crop&q=80&w=800", span: "md:col-span-1 md:row-span-1" },
-    { id: 3, img: "/internal_tranning.jpg", span: "md:col-span-1 md:row-span-1" },
+    { id: 1, img: "about-us/team_building.jpg", span: "md:col-span-2 md:row-span-2" },
+    { id: 2, img: "about-us/site_supervision.jpg", span: "md:col-span-1 md:row-span-1" },
+    { id: 3, img: "about-us/internal_tranning.jpg", span: "md:col-span-1 md:row-span-1" },
     { id: 4, img: "https://images.unsplash.com/photo-1600880292203-757bb62b4baf?auto=format&fit=crop&q=80&w=800", span: "md:col-span-1 md:row-span-2" },
     // { id: 5, img: "https://images.unsplash.com/photo-1508514177221-188b1cf16e9d?auto=format&fit=crop&q=80&w=800", span: "md:col-span-2 md:row-span-1" },
     { id: 6, img: "https://images.unsplash.com/photo-1762944082537-bf904828a5c9?auto=format&fit=crop&q=80&w=800", span: "md:col-span-2 md:row-span-1" },
